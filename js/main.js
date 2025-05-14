dccecd60c91c954d1b0d8ba796081f432cae8172
KaoriHasegawa <k04b187a@gmail.com>
2025-05-14 21:32:46 +0900

    レスポンシブル確認

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
   // カルーセル
-  
-    $('.slides').slick({
+$(document).ready(function () {
+  if (window.innerWidth <= 767) {
+    // SP用スライダーを初期化
+    $('.slider-sp').slick({
       autoplay: true,
       dots: true,
       infinite: true,
@@ -8,21 +10,32 @@
       arrows: false,
       cssEase: 'linear'
     });
-  
+  } else {
+    // PC用スライダーを初期化
+    $('.slider-pc').slick({
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      cssEase: 'linear'
+    });
+  }
+});
+
+//ヘッダー追従
+window.addEventListener("scroll", function () {
+  // ヘッダーを変数の中に格納する
+  const header = document.querySelector(".header");
+  // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
+  header.classList.toggle("scroll-nav", window.scrollY > 100);
+});
  
  
  // ハンバーガー   
- $("#hamburger-block").click( 
-  function() {
-    $(".nav-sp").toggle(300)
-  }
-);
-$("#hamburger-block").click(
-  function() {
-  $(".nav-sp").toggle(300)
-  $(".hamburger").toggleClass("active")
-  }
-  );
+$("#hamburger-block").click(function() {
+  $(".nav-sp").toggle(300); // メニュー表示/非表示
+  $(".hamburger").toggleClass("active"); // ハンバーガー見た目切り替え
+});
 
 
   // モーダル
@@ -53,3 +66,22 @@ $("#hamburger-block").click(
   $(".target").click(function() {
   $(this).css('color','red');
 });
+
+//TOPに戻る
+const scrollTopBtn = document.querySelector('.page-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 900) {
+      scrollTopBtn.classList.add('visible');
+    } else {
+      scrollTopBtn.classList.remove('visible');
+    }
+  });
+
+  scrollTopBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
